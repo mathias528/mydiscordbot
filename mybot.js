59b862089c6d7964527a76a7b7944fc201927115
mathias528 <32724423+mathias528@users.noreply.github.com>
2017-10-30 22:34:34 -0400

    Initial commit

---
+++ mybot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const fs = require("fs")
+	
+
+client.on("message", (message) => {
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("https://github.com/mathias528/Madden-Companion-Exporter/blob/master/madden-project-67765-firebase-adminsdk-q66tt-1aea6b2f42.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://madden-project-67765.firebaseio.com"
+});
+	
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+  if(command === 'ping') {
+  message.channel.send('Pong!');
+} else 
+if (command === 'blah') {
+  message.channel.send('Meh.');
+}
+});
+
+client.login(config.token);
